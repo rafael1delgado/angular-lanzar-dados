42059ce77033aea692c63a37c715639662e560b3
rafael1delgado <delgadorafael2011@gmail.com>
2021-11-07 19:56:27 -0400

    Cargar la app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'angular-dados';
+
+  dadoIzquierdo = '../assets/img/dice1.png';
+  dadoDerecho = '../assets/img/dice4.png';
+
+  numero1: number | undefined;
+  numero2: number | undefined;
+
+  ngOnInit(): void {
+    this.lanzarDados();
+  }
+
+  lanzarDados(): void {
+    this.numero1 = Math.round(Math.random() * 5) + 1;
+    this.numero2 = Math.round(Math.random() * 5) + 1;
+    this.dadoIzquierdo = '../assets/img/dice' + this.numero1 + '.png';
+    this.dadoDerecho = '../assets/img/dice' + this.numero2 + '.png';
+  }
 }
